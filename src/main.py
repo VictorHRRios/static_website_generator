4d7be596b82a08f56ce4e6d11f9c0d43d4972829
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-03-03 00:35:17 -0600

    add function to copy from direcory recurively

--- src/main.py
+++ src/main.py
@@ -3,6 +3,19 @@ import os
 import shutil
 from markdown import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dir_path_content):
+        raise Exception("directory to copy from don't exist")
+    list_of_files = os.listdir(dir_path_content)
+    for file in list_of_files:
+        source_file = os.path.join(dir_path_content, file)
+        destination_file = os.path.join(dest_dir_path, file)
+        if os.path.isfile(source_file):
+            generate_page(source_file, template_path, destination_file.replace('.md', '.html')) #change extension to html
+        else:
+            os.mkdir(destination_file)
+            generate_pages_recursive(source_file, template_path, destination_file)
+
 def generate_page(from_path, template_path, dest_path):
     print(f"generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as file:
@@ -15,10 +28,6 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Content }}", html_str)
     with open(dest_path, "w") as text_file:
         text_file.write(template_content)
-
-
-    
-
 
 def copy_contents(source, destination):
     if not os.path.exists(source):
@@ -40,13 +49,13 @@ def extract_title(markdown):
     lines = markdown.split('\n')
     for line in lines:
         if line[:2] == "# ":
-            return line
+            return line[2:]
     raise Exception("No header")
     
 def main():
     #sample = TextNode("this is a text node", TextType.BOLD, 'https://www.boot.dev')
     copy_contents("static", "public")
-    generate_page("content/index.md", 'template.html', 'public/index.html')
+    generate_pages_recursive("content", 'template.html', 'public')
     
     
 
